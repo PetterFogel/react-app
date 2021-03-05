13d246a2455d1929409ae8785912e088471fa4b5
Petter <petter.fogel@medieinstitutet.se>
2021-03-05 13:59:13 +0100

    initial-setup and add boilertemplates

--- index.tsx
+++ index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import App from './src/App';
 
 ReactDom.render(
-    <h1>Hello</h1>,
+    <App />,
     document.getElementById('root')
-);
+); 
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App() {
+    console.log("test");
+    return(
+        <h1>Hello World</h1>
+    ); 
+}
+
+export default App;
