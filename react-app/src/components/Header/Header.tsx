c21c7a08420c52da5a31ea41521aa204cd3dc4c9
Petter <petter.fogel@medieinstitutet.se>
2021-04-02 18:51:17 +0200

    Removed old styling and add new from mainStyle

--- react-app/src/components/Header/Header.tsx
+++ react-app/src/components/Header/Header.tsx
@@ -1,48 +1,19 @@
-import React, { CSSProperties } from 'react'
 import { Link } from 'react-router-dom'
+import { noDecoration } from '../../css/GeneralStyles';
+import { useStyles } from '../../css/HeaderStyle';
 import Navigation from './Navigation'
 
 function Header() {
+    const classes = useStyles();
     return (
-        <header style={rootStyle}>
+        <header className={classes.rootStyle}>
             <Link to="/" style={noDecoration}>
-                <h2 style={headerTitle}>React App</h2>
+                <h2 className={classes.headerLogo}>React App</h2>
             </Link>
             <Navigation/>
-            <h4 style={cartStyle}>Shopping Cart</h4>
+            <h4 className={classes.cartStyle}>Shopping Cart</h4>
         </header>
     )
-}
-
-const rootStyle: CSSProperties = {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: "0rem 2rem",
-    height: "7vh",
-    width: "100%",
-    position: "fixed",
-    top: 0,
-    left: 0,
-    right: 0,
-    borderBottom: "1px solid #222"
-    // background: "#222"
-}
-
-const noDecoration: CSSProperties = {
-    textDecoration: "none"
-}
-
-const headerTitle: CSSProperties = {
-    fontWeight: "normal",
-    color: "#222"
-}
-
-const cartStyle: CSSProperties = {
-    fontWeight: "normal",
-    background: "#333",
-    color: "#fff",
-    padding: "0.4rem 1rem"
 }
 
 export default Header
