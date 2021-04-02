938a7b55a82c31a22849bfd15cd9b1d452354711
Petter <petter.fogel@medieinstitutet.se>
2021-04-02 18:51:35 +0200

    Add content for homepage.

--- react-app/src/components/Main/Homepage.tsx
+++ react-app/src/components/Main/Homepage.tsx
@@ -1,13 +1,28 @@
 import React, { CSSProperties } from 'react'
+import { subTitle, title } from '../../css/GeneralStyles'
 
 function Homepage() {
     return (
-        <h1 style={title}>Homepage</h1>
+        <main style={rootStyle}>
+            <div>
+                <h2 style={title}>Homepage</h2>
+                <p style={subTitle}>test</p>
+            </div>
+            <div>
+                <p style={subTitle}>image</p>
+            </div>
+        </main>
     )
 }
 
-const title: CSSProperties = {
-    fontWeight: "normal"
+const rootStyle: CSSProperties = {
+    width: "100%",
+    height: "60vh",
+    background: "#c1e1ec",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
 }
 
 export default Homepage
