525a65cca9995754798bb0183fee7866049d98ca
Petter <petter.fogel@medieinstitutet.se>
2021-04-02 18:52:18 +0200

    Ass general titles styling

--- react-app/src/css/GeneralStyles.tsx
+++ react-app/src/css/GeneralStyles.tsx
@@ -3,3 +3,13 @@ import { CSSProperties } from "react";
 export const noDecoration: CSSProperties = {
     textDecoration: "none"
 }
+
+// HEADINGS
+export const title: CSSProperties = {
+    fontWeight: "normal",
+    fontSize: "2.5rem"
+}
+export const subTitle: CSSProperties = {
+    fontWeight: "normal",
+    fontSize: "1.5rem"
+}
