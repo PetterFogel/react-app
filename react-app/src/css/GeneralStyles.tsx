c96c6be675c9518fd1312e910681e420d40f1018
Petter <petter.fogel@medieinstitutet.se>
2021-04-05 18:44:43 +0200

    Add flexCenter generalstyle.

--- react-app/src/css/GeneralStyles.tsx
+++ react-app/src/css/GeneralStyles.tsx
@@ -4,10 +4,17 @@ export const noDecoration: CSSProperties = {
     textDecoration: "none"
 }
 
+export const flexCenter: CSSProperties = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+}
+
 // HEADINGS
 export const title: CSSProperties = {
     fontWeight: "normal",
-    fontSize: "2.5rem"
+    fontSize: "2.5rem",
+    margin: "1rem 0rem"
 }
 export const subTitle: CSSProperties = {
     fontWeight: "normal",
