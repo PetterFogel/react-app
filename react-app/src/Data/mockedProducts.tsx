48a1701675b0402f2998ccfae32ccbcdae215140
Petter <petter.fogel@medieinstitutet.se>
2021-04-07 21:49:52 +0200

    Add component with mocked products.

---
+++ react-app/src/Data/mockedProducts.tsx
@@ -0,0 +1,49 @@
+export interface Product {
+    name: string,
+    image: string,
+    price: number
+}
+
+export const products: Product[] = [
+    {
+        name: "bild1",
+        image: "https://images.pexels.com/photos/4753879/pexels-photo-4753879.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+        price: 1
+    },
+    {
+        name: "bild2",
+        image: "https://images.pexels.com/photos/4753879/pexels-photo-4753879.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+        price: 1
+    },
+    {
+        name: "bild3",
+        image: "https://images.pexels.com/photos/4753879/pexels-photo-4753879.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+        price: 1
+    },
+    {
+        name: "bild4",
+        image: "https://images.pexels.com/photos/4753879/pexels-photo-4753879.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+        price: 1
+    },
+    {
+        name: "bild5",
+        image: "https://images.pexels.com/photos/4753879/pexels-photo-4753879.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+
+        price: 1
+    },
+    {
+        name: "bild6",
+        image: "https://images.pexels.com/photos/4753879/pexels-photo-4753879.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+        price: 1
+    },
+    {
+        name: "bild6",
+        image: "https://images.pexels.com/photos/4753879/pexels-photo-4753879.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+        price: 1
+    },
+    {
+        name: "bild6",
+        image: "https://images.pexels.com/photos/4753879/pexels-photo-4753879.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+        price: 1
+    },
+]
