e335fcccd1f395088d73597dee046875a7a88e0f
Petter <petter.fogel@medieinstitutet.se>
2021-04-07 21:49:10 +0200

    Removed margin top

--- react-app/src/styles/MainStyle.tsx
+++ react-app/src/styles/MainStyle.tsx
@@ -3,7 +3,6 @@ import { createStyles, makeStyles, Theme } from "@material-ui/core";
 export const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         rootStyle: {
-            marginTop: "7vh",
             width: "100%",
         [theme.breakpoints.down('md')]: {
             width: "100%",
